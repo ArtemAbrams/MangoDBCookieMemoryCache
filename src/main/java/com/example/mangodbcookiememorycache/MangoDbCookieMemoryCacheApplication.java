12ee83933991ddd2596f289f890ce07496efb5da
artem <barishartem@gmail.com>
2023-11-02 20:41:39 +0200

    Initial project

---
+++ src/main/java/com/example/mangodbcookiememorycache/MangoDbCookieMemoryCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangodbcookiememorycache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoDbCookieMemoryCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoDbCookieMemoryCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mangodbcookiememorycache/MangoDbCookieMemoryCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangodbcookiememorycache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoDbCookieMemoryCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
